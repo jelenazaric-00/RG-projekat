fa900911558b5604f4cd7eab1fb302ebd3716bc5
abosiljcic <96130877+abosiljcic@users.noreply.github.com>
2022-04-30 18:57:18 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 bool ssaoButton=false;
+bool DSButton=false;
 bool blinn = false;
 bool blinnKeyPressed = false;
 bool flashLight = true;
@@ -105,9 +106,12 @@ int main(){
 
     //parallax mapping
     Shader parallaxShader("resources/shaders/parallaxMapping.vs", "resources/shaders/parallaxMapping.fs");
-    
     //normal mapping
     Shader normalShader("resources/shaders/normal_mapping.vs", "resources/shaders/normal_mapping.fs");
+    
+    //deferred shading
+    Shader shaderGeometryPassDS("resources/shaders/gBuffer.vs", "resources/shaders/gBuffer.fs");
+    Shader shaderLightingPassDS("resources/shaders/deferredShading.vs", "resources/shaders/deferredShading.fs");
 
 
     // set up vertex data (and buffer(s)) and configure vertex attrib
@@ -422,6 +426,73 @@ int main(){
     shaderSSAOBlur.use();
     shaderSSAOBlur.setInt("ssaoInput", 0);
 
+     //deferred shading
+    // configure g-buffer framebuffer
+    unsigned int gBufferDS;
+    glGenFramebuffers(1, &gBufferDS);
+    glBindFramebuffer(GL_FRAMEBUFFER, gBufferDS);
+    unsigned int gPositionDS, gNormalDS, gAlbedoSpecDS;
+    // position color buffer
+    glGenTextures(1, &gPositionDS);
+    glBindTexture(GL_TEXTURE_2D, gPositionDS);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gPositionDS, 0);
+    // normal color buffer
+    glGenTextures(1, &gNormalDS);
+    glBindTexture(GL_TEXTURE_2D, gNormalDS);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, gNormalDS, 0);
+    // color + specular color buffer
+    glGenTextures(1, &gAlbedoSpecDS);
+    glBindTexture(GL_TEXTURE_2D, gAlbedoSpecDS);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, gAlbedoSpecDS, 0);
+    // tell OpenGL which color attachments we'll use (of this framebuffer) for rendering
+    unsigned int attachmentsDS[3] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2 };
+    glDrawBuffers(3, attachmentsDS);
+    // create and attach depth buffer (renderbuffer)
+    unsigned int rboDepthDS;
+    glGenRenderbuffers(1, &rboDepthDS);
+    glBindRenderbuffer(GL_RENDERBUFFER, rboDepthDS);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepthDS);
+    // finally check if framebuffer is complete
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "Framebuffer not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+   // lighting info
+    const unsigned int NR_LIGHTS = 32;
+    std::vector<glm::vec3> lightPositions;
+    std::vector<glm::vec3> lightColors;
+    srand(13);
+    for (unsigned int i = 0; i < NR_LIGHTS; i++)
+    {
+        // calculate slightly random offsets
+        float xPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
+        float yPos = ((rand() % 100) / 100.0) * 6.0 - 4.0;
+        float zPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
+        lightPositions.push_back(glm::vec3(xPos, yPos, zPos));
+        // also calculate random color
+        float rColor = ((rand() % 100) / 200.0f) + 0.5;
+        float gColor = ((rand() % 100) / 200.0f) + 0.5;
+        float bColor = ((rand() % 100) / 200.0f) + 0.5;
+        lightColors.push_back(glm::vec3(rColor, gColor, bColor));
+    }
+
+    // shader configuration
+    shaderLightingPassDS.use();
+    shaderLightingPassDS.setInt("gPositionDS", 0);
+    shaderLightingPassDS.setInt("gNormalDS", 1);
+    shaderLightingPassDS.setInt("gAlbedoSpecDS", 2);
+
+    
     //render loop
     while (!glfwWindowShouldClose(window)){
         auto currentFrame = (float)glfwGetTime();
@@ -549,6 +620,100 @@ int main(){
             glBindTexture(GL_TEXTURE_2D, ssaoColorBufferBlur);
             renderQuad();
         }
+        else if(DSButton==true){
+            // 1. geometry pass: render scene's geometry/color data into gbuffer
+            // -----------------------------------------------------------------
+            glBindFramebuffer(GL_FRAMEBUFFER, gBufferDS);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            glm::mat4 view = camera.GetViewMatrix();
+            glm::mat4 model = glm::mat4(1.0f);
+            shaderGeometryPassDS.use();
+            shaderGeometryPassDS.setMat4("projection", projection);
+            shaderGeometryPassDS.setMat4("view", view);
+
+            // lightHouse
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, lighthousePosition);
+            model = glm ::rotate (model, glm::radians(10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::scale(model, glm::vec3(0.4f));
+            shaderGeometryPassDS.setMat4("model", model);
+            lightHouse.Draw(shaderGeometryPassDS);
+            //rocks
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(4, 0, -3));
+            model = glm::rotate(model, glm::radians(-15.0f), glm::vec3(0, 1, 0));
+            model = glm::scale(model, glm::vec3(1.8f));
+            shaderGeometryPassDS.setMat4("model", model);
+            rocks.Draw(shaderGeometryPassDS);
+            //boat
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, boatPosition);
+            if (front) {
+                model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0, 1.0, 0.0));
+            }
+            if (go_left) {
+                model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0, 1.0, 0.0));
+            }
+            if (go_right) {
+                model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0, 1.0, 0.0));
+            }
+            model = glm::scale(model, glm::vec3(0.02f));
+            shaderGeometryPassDS.setMat4("model", model);
+            Boat.Draw(shaderGeometryPassDS);
+
+            for (unsigned int i = 0; i < amt; i++) {
+                shaderGeometryPassDS.setMat4("model", modelMatrices[i]);
+                tail.Draw(shaderGeometryPassDS);
+            }
+            //big tail render
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, bigTailPosition);
+            model = glm::rotate(model, glm::radians(15.0f), glm::vec3(0, 1, -0.5));
+            model = glm::scale(model, glm::vec3(0.1f));
+            shaderGeometryPassDS.setMat4("model", model);
+            tail.Draw(shaderGeometryPassDS);
+
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+            // 2. lighting pass: calculate lighting by iterating over a screen filled quad pixel-by-pixel using the gbuffer's content.
+            // -----------------------------------------------------------------------------------------------------------------------
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            shaderLightingPassDS.use();
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, gPositionDS);
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, gNormalDS);
+            glActiveTexture(GL_TEXTURE2);
+            glBindTexture(GL_TEXTURE_2D, gAlbedoSpecDS);
+            // send light relevant uniforms
+            for (unsigned int i = 0; i < lightPositions.size(); i++)
+            {
+                shaderLightingPassDS.setVec3("lights[" + std::to_string(i) + "].Position", lightPositions[i]);
+                shaderLightingPassDS.setVec3("lights[" + std::to_string(i) + "].Color", lightColors[i]);
+                // update attenuation parameters and calculate radius
+                const float constant = 1.0; // note that we don't send this to the shader, we assume it is always 1.0 (in our case)
+                const float linear = 0.7;
+                const float quadratic = 1.8;
+                shaderLightingPassDS.setFloat("lights[" + std::to_string(i) + "].Linear", linear);
+                shaderLightingPassDS.setFloat("lights[" + std::to_string(i) + "].Quadratic", quadratic);
+                // then calculate radius of light volume/sphere
+                const float maxBrightness = std::fmaxf(std::fmaxf(lightColors[i].r, lightColors[i].g), lightColors[i].b);
+                float radius = (-linear + std::sqrt(linear * linear - 4 * quadratic * (constant - (256.0f / 5.0f) * maxBrightness))) / (2.0f * quadratic);
+                shaderLightingPassDS.setFloat("lights[" + std::to_string(i) + "].Radius", radius);
+            }
+            shaderLightingPassDS.setVec3("viewPos", camera.Position);
+            // finally render quad
+            renderQuad();
+
+            // 2.5. copy content of geometry's depth buffer to default framebuffer's depth buffer
+            glBindFramebuffer(GL_READ_FRAMEBUFFER, gBufferDS);
+            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0); // write to default framebuffer
+            // blit to default framebuffer
+            glBlitFramebuffer(0, 0, SCR_WIDTH, SCR_HEIGHT, 0, 0, SCR_WIDTH, SCR_HEIGHT, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        
         else {
             // lightshowShader setup
             lightShader.use();
@@ -883,6 +1048,12 @@ void processInput(GLFWwindow *window) {
     }
     if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_RELEASE){
         ssaoButton=false;
+    }
+    if(glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS){
+        DSButton=true;
+    }
+    if(glfwGetKey(window, GLFW_KEY_P) == GLFW_RELEASE){
+        DSButton=false;
     }
 }
 
