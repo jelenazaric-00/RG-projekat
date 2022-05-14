70e70fe54bbbc48907ce417c052f1650ea8ba9e2
abosiljcic <96130877+abosiljcic@users.noreply.github.com>
2022-05-14 19:25:34 +0200

    Update deferredShading.fs

--- resources/shaders/deferredShading.fs
+++ resources/shaders/deferredShading.fs
@@ -6,6 +6,7 @@ in vec2 TexCoords;
 uniform sampler2D gPositionDS;
 uniform sampler2D gNormalDS;
 uniform sampler2D gAlbedoSpecDS;
+uniform sampler2D ssao;
 
 struct Light {
     vec3 Position;
@@ -26,9 +27,11 @@ void main()
     vec3 Normal = texture(gNormalDS, TexCoords).rgb;
     vec3 Diffuse = texture(gAlbedoSpecDS, TexCoords).rgb;
     float Specular = texture(gAlbedoSpecDS, TexCoords).a;
+    float AmbientOcclusion = texture(ssao, TexCoords).r;
     
     // then calculate lighting as usual
-    vec3 lighting  = Diffuse * 0.1; // hard-coded ambient component
+    vec3 ambient = vec3(0.3 * Diffuse * AmbientOcclusion);
+    vec3 lighting  = ambient;
     vec3 viewDir  = normalize(viewPos - FragPos);
     for(int i = 0; i < NR_LIGHTS; ++i)
     {
@@ -40,9 +43,10 @@ void main()
             vec3 lightDir = normalize(lights[i].Position - FragPos);
             vec3 diffuse = max(dot(Normal, lightDir), 0.0) * Diffuse * lights[i].Color;
             // specular
-            vec3 halfwayDir = normalize(lightDir + viewDir);  
+            vec3 halfwayDir = normalize(lightDir + viewDir);
             float spec = pow(max(dot(Normal, halfwayDir), 0.0), 16.0);
             vec3 specular = lights[i].Color * spec * Specular;
+
             // attenuation
             float attenuation = 1.0 / (1.0 + lights[i].Linear * distance + lights[i].Quadratic * distance * distance);
             diffuse *= attenuation;
